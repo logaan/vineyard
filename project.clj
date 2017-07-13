dd5850ff062d4f3fbe9e1102da3276fed2d6fa5d
Logan Campbell <colin@logaan.net>
2017-07-14 00:10:00 +1000

    project description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject vineyard "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A small lisp with blocking concurrency semantics and threads
+                that compiles to Javascript."
+  :url "https://github.com/logaan/vineyard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
