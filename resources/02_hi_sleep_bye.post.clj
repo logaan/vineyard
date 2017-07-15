8792671867862a1190c6e12c81fc1c001a431a42
Logan Campbell <colin@logaan.net>
2017-07-15 15:59:51 +1000

    post processed with anon func

--- resources/02_hi_sleep_bye.post.clj
+++ resources/02_hi_sleep_bye.post.clj
@@ -1,6 +1,12 @@
-[#vineyard.core.Call{:fn-name   "log"
-                     :arguments [#vineyard.core.Text{:text "hi"}]}
- #vineyard.core.Call{:fn-name   "sleep"
-                     :arguments []}
- #vineyard.core.Call{:fn-name   "log"
-                     :arguments [#vineyard.core.Text{:text "bye"}]}]
+[#vineyard.core.Call
+ {:fn-name   "log"
+  :arguments [#vineyard.core.Text
+              {:text "hi"}]}
+ #vineyard.core.Call
+ {:fn-name   "sleep"
+  :arguments [#vineyard.core.AnonymousFunction
+              {:parameters []
+               :body [#vineyard.core.Call
+                      {:fn-name   "log"
+                       :arguments [#vineyard.core.Text
+                                   {:text "bye"}]}]}]}]
