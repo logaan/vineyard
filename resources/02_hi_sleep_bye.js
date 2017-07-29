c18a4f2dae67cab677135ba6b556beb2a56f5d24
Logan Campbell <colin@logaan.net>
2017-07-30 01:36:12 +1000

    unified testing method

--- resources/02_hi_sleep_bye.js
+++
@@ -1,4 +0,0 @@
-log("hi");
-sleep(function() {
-  log("bye")
-})
--- test/vineyard/block_test.clj
+++ test/vineyard/block_test.clj
@@ -14,28 +14,16 @@
   (is (not (sut/blocking-call? (data/call "log" []))))
   (is (not (sut/blocking-call? (data/text "kittens")))))
 
-(defn hi-sleep-bye-pre []
-  (read-string (slurp (io/resource "02_hi_sleep_bye.pre.clj"))))
+(def hi-sleep-bye-pre
+  [(data/call "log" [(data/text "hi")])
+   (data/call "sleep" [])
+   (data/call "log" [(data/text "bye")])])
 
 (deftest finds-blocking
   (is (= [[(data/call "log" [(data/text "hi")])]
           (data/call "sleep" [])
           [(data/call "log" [(data/text "bye")])]]
-         (sut/find-blocking (hi-sleep-bye-pre)))))
-
-(defn hi-sleep-bye-post []
-  (read-string (slurp (io/resource "02_hi_sleep_bye.post.clj"))))
-
-(deftest passes-continuation-to-first-blocking
-  (is (= (hi-sleep-bye-post)
-         (sut/make-blocking (hi-sleep-bye-pre)))))
-
-(defn hello-world []
-  (read-string (slurp (io/resource "01_hello_world.clj"))))
-
-(deftest leaves-non-blocking-alone []
-  (is (= (hello-world)
-         (sut/make-blocking (hello-world)))))
+         (sut/find-blocking hi-sleep-bye-pre))))
 
 (defn read-test-data [directory]
   (letfn [(slurp-res [filename] (slurp (io/resource (str directory filename))))]
@@ -57,4 +45,6 @@
     (is (= (test-helpers/error-free (:output expected)) output))))
 
 (deftest runs-multiple
+  (run-from-test-data "01_hello_world/")
+  (run-from-test-data "02_hi_sleep_bye/")
   (run-from-test-data "03_hi_sleep_ok_sleep_bye/"))
